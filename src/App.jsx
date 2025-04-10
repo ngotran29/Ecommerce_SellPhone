0ff4bf93351bed64e93f37b61fb80dc6da20f3c5
ngotran29 <ngotran296yh@gmail.com>
2025-04-11 03:05:14 +0700

    router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,31 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from '../redux/slides/counterSlice'
-import styled from 'styled-components'; 
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { routes } from '../routes'
+import { HeaderComponent } from '../components/HeaderComponent/HeaderComponent'
+import DefaultComponent from '../components/DefaultComponent/DefaultComponent'
 
 function App() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
-
-  const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-// Create a <Wrapper> react component that renders a <section> with
-// some padding and a papayawhip background
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
-
+ 
   return (
     
     <div>
-       <Wrapper>
-      <Title>Hello World, this is my first styled component!</Title>
-    </Wrapper>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
+     
+      <Router>
+        <Routes>
+          {routes.map((route) =>{
+            const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+            return(
+              <Route path={route.path} element= {
+                <Layout>
+                  <Page/>
+                </Layout>
+              }/>
+            )
+          })}
+        </Routes>
+      </Router>
+      
     </div>
   )
 }
